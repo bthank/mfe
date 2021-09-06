2dc337051af567aa7606838be281f0275a193aee
bthank <bthankachan@yahoo.com>
2021-09-06 17:02:14 -0400

    Finished feature #1

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -7,7 +7,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
             <div>
-                <h1>Hello there I am coming from App.js!</h1>
+                <h1>Hello there I am coming from App.js!!!!!!!</h1>
                 <hr/>
                 <MarketingApp />
             </div>
