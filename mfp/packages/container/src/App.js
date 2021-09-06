8d3b338fffdb4d43752940cd19ffc5f9808bbf5f
bthank <bthankachan@yahoo.com>
2021-09-06 17:27:23 -0400

    Added header

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 //import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 //console.log(mount)
 
 export default () => {
     return (
-            <div>
-                <h1>Hello there I am coming from App.js!!!!!!!</h1>
-                <hr/>
-                <MarketingApp />
-            </div>
+            <BrowserRouter>       
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
             ); 
     
     
